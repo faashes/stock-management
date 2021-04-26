9aca31a3a79677e6943ad06a592d05a4bba802bf
faashes <aashaychaudhari@gmail.com>
2021-04-26 23:00:18 +0530

    Initial commit

--- project/settings.py
+++ project/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'storedb'
+    'storedb',
+    'django_truncate',
 ]
 
 MIDDLEWARE = [
